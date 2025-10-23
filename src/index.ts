52c5770508d1d14c74007bae8b4bcd8abfbfebaf
Vitor <vitorhugosnp@gmail.com>
2025-10-23 18:22:05 -0300

    feat: added cors and openAPI private to only dev enviroment

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import { authController } from "./features/auth";
 import openapi, { fromTypes } from "@elysiajs/openapi";
 import { userController } from "./features/user";
 import { rankingController } from "./features/ranking";
+import cors from "@elysiajs/cors";
 
 const app = new Elysia()
-  .use(openapi())
+  .use(openapi({ enabled: !!!Bun.env.NODE_ENV }))
+  .use(
+    cors({
+      origin: /^(?:www\.)?fubaclicker\.com\.br$/,
+    }),
+  )
   .use(userController)
   .use(authController)
   .use(rankingController)
