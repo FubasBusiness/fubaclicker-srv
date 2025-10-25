4dd7a47356b0aa0579b545e265d3a0554ccded1d
Vitor <vitorhugosnp@gmail.com>
2025-10-25 03:03:52 -0300

    chore: removed error handling to bubble to the top

--- src/features/user/use-cases/update-user.use-case.ts
+++ src/features/user/use-cases/update-user.use-case.ts
@@ -2,9 +2,5 @@ import { db } from "../../../db";
 import { User, users } from "../../../db/schema/user";
 
 export async function UpdateUser(data: Partial<User>) {
-  try {
-    await db.update(users).set(data);
-  } catch (e) {
-    throw Error("Unknown server error");
-  }
+  await db.update(users).set(data);
 }
