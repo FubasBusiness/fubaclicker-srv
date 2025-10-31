98406ba15e4a18ea2413448c119ca31e80934857
Vitor <vitorhugosnp@gmail.com>
2025-10-31 01:04:24 -0300

    fix: removed timeout

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,16 +3,9 @@ import { users } from "./schema/user";
 import { refreshTokens } from "./schema/refresh-tokens";
 import postgres from "postgres";
 
-const ca = Bun.file("/etc/ssl/certs/ca-certificates.crt", { type: "utf8" });
-const client = postgres(
-  Bun.env.FubaDB!.includes("?")
-    ? Bun.env.FubaDB! + "&sslmode=require"
-    : Bun.env.FubaDB! + "?sslmode=require",
-  {
-    idle_timeout: 1200,
-    ssl: { ca },
-  },
-);
+const client = postgres(Bun.env.FubaDB!, {
+  ssl: true,
+});
 
 export const db = drizzle({
   client,
